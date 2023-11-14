4881d61d174e69bac1a23d3b5ca0c1cbc42bfaab
KERRCAM <90578826+KERRCAM@users.noreply.github.com>
2023-11-14 17:14:55 +0000

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/NesC__.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NesC__.dir/main.cpp.obj"
+  "NesC__.exe"
+  "NesC__.exe.manifest"
+  "NesC__.lib"
+  "NesC__.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NesC__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/NesC__.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for NesC__.
+# This may be replaced when dependencies are built.
